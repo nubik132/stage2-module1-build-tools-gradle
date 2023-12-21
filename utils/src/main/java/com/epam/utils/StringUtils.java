b4bb2038ec7f770bb0a84cf90f93bd5e89bd8ad2
nubik132 <mister.nubik@list.ru>
2023-12-21 17:08:27 +0300

    final fix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         return Integer.parseInt(str) > 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
